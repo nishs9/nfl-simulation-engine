7405d8a472aa40e4ed676d8a951ccea8f0808359
Nishant Sethunath <snishk@gmail.com>
2024-11-25 17:06:48 -0800

    additional setup of backend virtual env and dependencies

---
+++ backend/test_db_connection.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from secrets import db_username, db_password, db_name
+
+connection_string = f'mysql+pymysql://{db_username}:{db_password}@localhost/{db_name}'
+engine = create_engine(connection_string, echo=True)
+
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+except Exception as e:
+    print(f"Connection failed: {e}")
